7e17e4f8e81956a42569bc55136f2756f1a98e7a
Anurag yadav <anuragyadav622003@gmail.com>
2025-02-27 22:51:46 +0530

    ch

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-import Layout from "./components/Layout";
-import NotFound from "./pages/NotFound";
+import Home from "../pages/Home";
+
+// import About from "./pages/About";
+// import Contact from "./pages/Contact";
+// import Layout from "./components/Layout";
+// import NotFound from "./pages/NotFound";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />, // Wrapper component (e.g., Navbar, Footer)
     children: [
-      { path: "", element: <Home /> },
-      { path: "about", element: <About /> },
-      { path: "contact", element: <Contact /> },
+      { path: "", element: <Home/> },
+  
     ],
   },
   { path: "*", element: <NotFound /> }, // 404 Page
